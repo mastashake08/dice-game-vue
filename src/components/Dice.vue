1a30d01caa3ff815a3a35ab7c7181d292674a0a9
Hacker God <jyrone.parker@gmail.com>
2023-05-11 03:04:44 -0400

    GPT Dice game

---
+++ src/components/Dice.vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="dice" :class="{ 'rolling': rolling }" @dice-clicked="roll">
+    <svg class="face" :class="'face-' + value" viewBox="0 0 100 100">
+      <circle cx="50" cy="50" r="12" v-if="value === 1" />
+      <circle cx="30" cy="30" r="12" v-if="[2, 3, 4, 5, 6].includes(value)" />
+      <circle cx="70" cy="50" r="12" v-if="[2, 3, 4, 5, 6].includes(value)" />
+      <circle cx="30" cy="70" r="12" v-if="[3, 4, 5, 6].includes(value)" />
+      <circle cx="70" cy="30" r="12" v-if="[4, 5, 6].includes(value)" />
+      <circle cx="30" cy="50" r="12" v-if="[5,6].includes(value)" />
+      <circle cx="70" cy="50" r="12" v-if="[6].includes(value)" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 1,
+      rolling: false
+    };
+  },
+  methods: {
+    roll() {
+      // Generate a random number between 1 and 6
+      const randomValue = Math.floor(Math.random() * 6) + 1;
+
+      // Set the new value and start the rolling animation
+      this.value = randomValue;
+      this.rolling = true;
+      this.$emit('dice-rolling', this.rolling)
+      // Wait for the rolling animation to finish
+      setTimeout(() => {
+        this.rolling = false;
+        this.$emit('dice-rolled', this.value);
+      }, 1000);
+    }
+  }
+};
+</script>
+
+<style>
+.dice {
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  position: relative;
+  border-radius: 10px;
+  border: 2px solid black;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.dice.rolling {
+  animation: rolling 1s linear infinite;
+}
+
+.dice .face {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transform: translateX(0);
+  transition: transform 1s;
+}
+
+.dice .face circle {
+  fill: black;
+}
+
+.dice .face-1 circle:nth-of-type(1) {
+  cx: 50;
+  cy: 50;
+}
+
+.dice .face-2 circle:nth-of-type(1) {
+  cx: 30;
+  cy: 30;
+}
+
+.dice .face-2 circle:nth-of-type(2) {
+  cx: 70;
+  cy: 70;
+}
+
+.dice .face-3 circle:nth-of-type(1) {
+  cx: 30;
+  cy: 30;
+}
+
+.dice .face-3 circle:nth-of-type(2) {
+  cx: 50;
+  cy: 50;
+}
+
+.dice .face-3 circle:nth-of-type(3) {
+  cx: 70;
+  cy: 70;
+}
+
+.dice .face-4 circle:nth-of-type(1) {
+  cx: 30;
+  cy: 30;
+}
+
+.dice .face-4 circle:nth-of-type(2) {
+  cx: 70;
+  cy: 70;
+}
+
+.dice .face-4 circle:nth-of-type(3) {
+  cx: 30;
+  cy: 70;
+}
+
+.dice .face-4 circle:nth-of-type(4) {
+  cx: 70;
+  cy: 30;
+}
+
+.dice .face-5 circle:nth-of-type(1) {
+  cx: 30;
+  cy: 30;
+}
+
+.dice .face-5 circle:nth-of-type(2) {
+  cx: 70;
+  cy: 70;
+}
+
+.dice .face-5 circle:nth-of-type(3) {
+  cx: 50;
+  cy: 50;
+}
+
+.dice .face-5 circle:nth-of-type(4) {
+  cx: 70;
+  cy: 30;
+}
+
+.dice .face-5 circle:nth-of-type(5) {
+  cx: 30;
+  cy: 70;
+}
+
+.dice .face-6 circle:nth-of-type(1) {
+  cx: 30;
+  cy: 30;
+}
+
+.dice .face-6 circle:nth-of-type(2) {
+  cx: 70;
+  cy: 70;
+}
+
+.dice .face-6 circle:nth-of-type(3) {
+  cx: 30;
+  cy: 50;
+}
+
+.dice .face-6 circle:nth-of-type(4) {
+  cx: 70;
+  cy: 50;
+}
+
+.dice .face-6 circle:nth-of-type(5) {
+  cx: 30;
+  cy: 70;
+}
+
+.dice .face-6 circle:nth-of-type(6) {
+  cx: 70;
+  cy: 30;
+}
+
+@keyframes rolling {
+  0% {
+    transform: translateX(0);
+  }
+  25% {
+    transform: translateX(-25%);
+  }
+  50% {
+    transform: translateX(25%);
+  }
+  75% {
+    transform: translateX(-25%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,126 @@
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="dice-container">
+    <div class="wager-container">
+      <label for="wager-input">Enter your wager:</label>
+      <input id="wager-input" type="number" v-model="wager" />
+    </div>
+    <div class="dice-wrapper">
+      <Dice @dice-rolled="handleDiceRolled1" ref="d1" @dice-rolling="handleDiceRolling1"/>
+      <Dice @dice-rolled="handleDiceRolled2" ref="d2" @dice-rolling="handleDiceRolling1"/>
+
+    </div>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Your point: {{currentPoint}}
+      <button @click="rollDice"> Roll Dice</button>
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
 <script>
+import Dice from './Dice'
 export default {
   name: 'HelloWorld',
+  data () {
+    return {
+      d1: 0,
+      d1r: false,
+      dr2: false,
+      d2: 0,
+      pointSet: false,
+      point: 0,
+      wager: 0
+    }
+  },
   props: {
     msg: String
+  },
+  components: {
+    Dice
+  },
+  computed: {
+    currentPoint() {
+      if (this.d1 === 0 || this.d2 === 0) {
+        return 0
+      } else {
+        return this.point
+      }
+    }
+  },
+  methods: {
+    rollDice() {
+      this.$refs.d1.roll()
+      this.$refs.d2.roll()
+    },
+    setPoint () {
+      if (this.d1 !== 0 && this.d2 !== 0) {
+      if (!this.pointSet) {
+        this.pointSet = true
+        this.point = this.d1 + this.d2
+        if(this.point === 7 || this.point === 11) {
+          alert('YOU WIN')
+          this.d1 = 0
+          this.d2 = 0
+          this.point = 0
+          this.pointSet = false
+        }
+      } else if(this.currentPoint === this.d1 + this.d2) {
+          alert('YOU WIN!')
+          this.d1 = 0
+          this.d2 = 0
+          this.point = 0
+          this.pointSet = false
+        } else if (this.d1 + this.d2 == 7 && (this.d1r == true && this.d2r == true)) {
+          alert('YOU LOSE!')
+          this.d1 = 0
+          this.d2 = 0
+          this.point = 0
+          this.pointSet = false
+        }
+      }
+    },
+    handleDiceRolled1(val) {
+      console.log('Dice Rolled: ' + val)
+
+      this.d1 = val
+      this.d1r = false
+      this.setPoint()
+    },
+    handleDiceRolled2(val) {
+      console.log('Dice Rolled: ' + val)
+      this.d2 = val
+      this.d2r = false
+      this.setPoint()
+    },
+    handleDiceRolling1(val) {
+      console.log('Dice rolling: ' + val)
+      this.d1r = true
+    },
+    handleDiceRolling2(val) {
+      console.log('Dice rolling: ' + val)
+      this.d22 = true
+    }
   }
 }
 </script>
+<style>
+.dice-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+.wager-container {
+  margin-bottom: 2rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.dice-wrapper {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
